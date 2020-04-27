b2680014b6eeeb239ea90ccd40d8fba0b3ee3235
bleazard <thomasbleazard@hotmail.com>
2020-04-27 11:47:39 +0100

    roxygen2 additions

--- R/makebarplot.R
+++ R/makebarplot.R
@@ -1,4 +1,11 @@
+#' Make a simple bar plot
+#'
+#' @param mydf A dataframe with an X and a Y column
+#' @return A bar plot in ggplot ready to plot
+#' @examples
+#' makebarplot(mydataframe)
 makebarplot <- function(mydf, ...) {
-  ggplot(data=mydf) +
-    geom_bar(aes(x=X, y=Y), stat="identity")
+  (p<-ggplot(data=mydf) +
+    geom_bar(aes(x=X, y=Y), stat="identity"))
+  return(p)
 }
